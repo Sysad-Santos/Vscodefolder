f1b098e7b8ddfde5c918b1209bd66cd5ea963f17
sysad-santos <sysad.santos@gmail.com>
2022-01-18 06:46:20 -0800

    addedcode

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
+from pprint import pprint
+from re import M
+
+
 print("Hello, World")
+myValue=1
+print(myValue)
+pprint(type)(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
